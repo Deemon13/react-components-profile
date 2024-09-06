6dc39690b754e222da64aed82d74958e0d2c841e
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2024-09-06 18:49:25 +0300

    added styles to friends component

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -6,12 +6,12 @@ export const FriendList = props => {
 	const { friends } = props;
 
 	return (
-		<ul>
+		<ul className={styles.friends}>
 			{friends.map(friend => {
 				const { avatar, name, isOnline } = friend;
 
 				return (
-					<li key={friend.id}>
+					<li key={friend.id} className={styles.friends__item}>
 						<FriendListItem avatar={avatar} name={name} isOnline={isOnline} />
 					</li>
 				);
--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,9 +1,19 @@
+import styles from './FriendListItem.module.css';
+
 export const FriendListItem = ({ avatar, name, isOnline }) => {
 	return (
-		<div>
-			<img src={avatar} alt="Avatar" width="48" />
-			<p>{name}</p>
-			<p>{isOnline ? 'Online' : 'Offline'}</p>
-		</div>
+		<>
+			<img src={avatar} className={styles.friend__avatar} alt="Avatar" width="48" />
+			<p className={styles.friend__name}>{name}</p>
+			<p
+				className={`${styles.friend__status} ${
+					styles[
+						isOnline ? 'friend__status--online' : 'friend__status--offline'
+					]
+				}`}
+			>
+				{isOnline ? 'Online' : 'Offline'}
+			</p>
+		</>
 	);
 };
